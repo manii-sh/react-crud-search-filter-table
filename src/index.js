9365981002b948e18cf80299ad00bc613b78fe1d
manii-sh <hugmeforever856@gmail.com>
2022-09-26 13:28:00 +0530

    redux-persist added

--- src/index.js
+++ src/index.js
@@ -5,13 +5,19 @@ import App from './App';
 import { BrowserRouter } from "react-router-dom";
 import {Provider} from 'react-redux'
 import store from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+
+let persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,21 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import {persistReducer} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import employeeReducer from "../redux/reducers/employeeReducer";
 
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const reducer = combineReducers({
+  employees: employeeReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
 const store = configureStore({
-  reducer: {
-    employees: employeeReducer,
-  },
+  reducer: persistedReducer,
 });
 
 export default store;
